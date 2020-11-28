505a5fc435381e9512c658c5eea68ec1444b75c9
neh <psdafuq@gmail.com>
2020-11-28 20:17:44 -0300

    Update GrantAccessCommand.java

--- src/main/java/factionsystem/Commands/GrantAccessCommand.java
+++ src/main/java/factionsystem/Commands/GrantAccessCommand.java
@@ -24,7 +24,7 @@ public class GrantAccessCommand {
 
                 // if args[1] is cancel, cancel this
                 if (args[1].equalsIgnoreCase("cancel")) {
-                    player.sendMessage(ChatColor.GREEN + "Command cancelled!");
+                    player.sendMessage(ChatColor.GREEN + "¡Comando cancelado!");
                     return;
                 }
 
@@ -32,14 +32,14 @@ public class GrantAccessCommand {
                 if (!main.playersGrantingAccess.containsKey(player.getUniqueId())) {
                     // save target name and player name in hashmap in main
                     main.playersGrantingAccess.put(player.getUniqueId(), findUUIDBasedOnPlayerName(args[1]));
-                    player.sendMessage(ChatColor.GREEN + "Right click a chest or door to grant " + args[1] + " access. Type /mf grantaccess cancel to cancel this.");
+                    player.sendMessage(ChatColor.GREEN + "Click derecho en un cofre o puerta para darle a " + args[1] + " acceso. Escribe /mf grantaccess cancel para cancelar.");
                 }
                 else {
-                    player.sendMessage(ChatColor.RED + "You are already granting access to someone! Type /mf grantaccess cancel to cancel this.");
+                    player.sendMessage(ChatColor.RED + "¡Ya le estás dando acceso a alguien! Escribe /mf grantaccess cancel para cancelar.");
                 }
             }
             else {
-                player.sendMessage(ChatColor.RED + "Usage: /grantaccess (player-name)");
+                player.sendMessage(ChatColor.RED + "Uso: /grantaccess (nombre-jugador)");
             }
 
         }
